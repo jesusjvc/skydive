80c5fd3cc9a4fb43d60ae4369fb16ccb0a4a05fc
marhowlett <marimar_r2003@hotmail.com>
2020-06-17 18:47:11 -0500

    blog

--- loop.php
+++ loop.php
@@ -1,33 +1,56 @@
-<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-
+<div class="contenedor-blog">
+    <?php
+    $bar = 0;
+    
+   
+    if (have_posts()): while (have_posts()) : the_post(); ?>
+    <?php
+$bar=$bar+1;
+    ?>
 	<!-- article -->
+	
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
+        
 		<!-- post thumbnail -->
-		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
+		<?php
+      
+        if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
 			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-				<?php the_post_thumbnail(array(120,120)); // Declare pixel size you need inside the array ?>
+				<?php the_post_thumbnail(array(668,376)); // Declare pixel size you need inside the array ?>
 			</a>
 		<?php endif; ?>
 		<!-- /post thumbnail -->
 
-		<!-- post title -->
-		<h2>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-		</h2>
-		<!-- /post title -->
+		
 
 		<!-- post details -->
-		<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
+		<div class="datos">
+		
 		<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
+		<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
 		<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
 		<!-- /post details -->
-
+        </div>
+        <!-- post title -->
+		<h3>
+			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+		</h3>
+		<!-- /post title -->
 		<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
 
-		<?php edit_post_link(); ?>
+		<?php // edit_post_link(); ?>
 
 	</article>
+	
+	<?php
+    if($bar==2){
+      
+         ?>
+        </div>
+        <div class="contenedor-blog">
+        <?php
+    }
+    ?>
 	<!-- /article -->
 
 <?php endwhile; ?>
@@ -41,3 +64,4 @@
 	<!-- /article -->
 
 <?php endif; ?>
+
--- single.php
+++ single.php
@@ -3,7 +3,7 @@
 	<main role="main">
 	<!-- section -->
 	<section>
-
+<div class="contenedor-blog">
 	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
 		<!-- article -->
@@ -18,15 +18,17 @@
 			<!-- /post thumbnail -->
 
 			<!-- post title -->
-			<h1>
+			<h2>
 				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-			</h1>
+			</h2>
 			<!-- /post title -->
 
 			<!-- post details -->
+			<div class="datos">
 			<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
 			<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
 			<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
+			</div>
 			<!-- /post details -->
 
 			<?php the_content(); // Dynamic Content ?>
@@ -35,7 +37,7 @@
 
 			<p><?php _e( 'Categorised in: ', 'html5blank' ); the_category(', '); // Separated by commas ?></p>
 
-			<p><?php _e( 'This post was written by ', 'html5blank' ); the_author(); ?></p>
+			<p><?php _e( 'Este artículo fue escrito por:   ', 'html5blank' ); the_author(); ?></p>
 
 			<?php edit_post_link(); // Always handy to have Edit Post Links available ?>
 
@@ -43,7 +45,7 @@
 
 		</article>
 		<!-- /article -->
-
+        </div>
 	<?php endwhile; ?>
 
 	<?php else: ?>
