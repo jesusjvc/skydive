93761eeafc1097b70193bbec743690c142d4a03e
marhowlett <marimar_r2003@hotmail.com>
2021-04-14 12:20:02 -0500

    sobre el nivel del mar

--- plantillas/home.php
+++ plantillas/home.php
@@ -107,7 +107,7 @@ get_header();
             <div class="fondo">
                 <h3>
                     13 MIL<span>ft</span>
-                    <span class="snm">SNM</span>
+                    <span class="snm">sobre el nivel medio del mar</span>
                     <div class="precio">$1,590</div>
                 </h3>
             </div>
@@ -124,7 +124,7 @@ get_header();
             <div class="fondo">
                 <h3>
                     15 MIL<span>ft</span>
-                    <span class="snm">SNM</span>
+                    <span class="snm">sobre el nivel medio del mar</span>
                     <div class="precio">$1,890</div>
                 </h3>
             </div>
@@ -143,7 +143,7 @@ get_header();
             <div class="fondo">
                 <h3>
                     17 MIL<span>ft</span>
-                    <span class="snm">SNM</span>
+                    <span class="snm">sobre el nivel medio del mar</span>
                     <div class="precio">$2,190</div>
                 </h3>
             </div>
